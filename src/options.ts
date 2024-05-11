e7c06fc221b554ba280947d35a743ac5ea593e17
Alaa Elsaid <62419181+alaa-hany-elsaid@users.noreply.github.com>
2024-05-11 19:52:16 +0300

    Update options.ts
    Add OnSave and OnCancel to sync content in SPA libs 
    Like Vue And NG

--- src/options.ts
+++ src/options.ts
@@ -16,4 +16,6 @@ export interface QuillHtmlEditButtonOptions {
   editorModules?: {
     // Any modules here will be declared in HTML quill editor instance
   };
+  onSave?() : void;
+  onCancel?() : void;
 }
